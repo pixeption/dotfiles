f6884fe82c0b4c88edcf9b6306edba40b6ab2676
Pixeption <pixeption@gmail.com>
2024-11-21 14:09:56 +0700

    kitty: vim-navigator

---
+++ nvim/lua/plugins/kitty-navigator.lua
@@ -0,0 +1,26 @@
+return {
+  "knubie/vim-kitty-navigator",
+  enabled = true,
+  keys = {
+    {
+      "<C-h>",
+      "<cmd>KittyNavigateLeft<cr>",
+      desc = "Navigate window left",
+    },
+    {
+      "<C-j>",
+      "<cmd>KittyNavigateDown<cr>",
+      desc = "Navigate window down",
+    },
+    {
+      "<C-k>",
+      "<cmd>KittyNavigateUp<cr>",
+      desc = "Navigate window up",
+    },
+    {
+      "<C-l>",
+      "<cmd>KittyNavigateRight<cr>",
+      desc = "Navigate window right",
+    },
+  },
+}
