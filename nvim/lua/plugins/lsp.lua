5b03f4efd7655c18cffb01364a0d8ab90f0609d9
Pixeption <pixeption@gmail.com>
2026-07-16 09:32:37 +0700

    trackball: various update related to trackball

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     servers = {
       omnisharp = {
-        autostart = false,
+        enabled = false,
         enable_roslyn_analyzers = true,
         organize_imports_on_format = true,
         enable_import_completion = true,
